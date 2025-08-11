5a8b23be4d1c64fec7c7d2b0d21f8c4071c55da8
nirmalsimon <nirmalsimon333@gmail.com>
2025-08-12 02:38:09 +0530

    Logging

--- src/main/java/onboarding/Service/CustomerService.java
+++ src/main/java/onboarding/Service/CustomerService.java
@@ -2,22 +2,28 @@ package onboarding.Service;
 
 import onboarding.entity.Customer;
 import onboarding.repository.CustomerRepository;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.slf4j.Logger.*;
 import java.util.Optional;
 
 @Service
 @Transactional
 public class CustomerService {
 
+    private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
     @Autowired
     private CustomerRepository customerRepository;
 
     public Customer saveCustomer(Customer customer) {
         customer.setKycStatus("PENDING"); // Default status
+        log.info("Here ");
         return customerRepository.save(customer);
+
     }
 
 
