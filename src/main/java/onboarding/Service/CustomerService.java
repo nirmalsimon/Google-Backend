cb993ffba5eace21beb0f1791830ff4e540c9cfe
nirmalsimon <nirmalsimon333@gmail.com>
2025-08-12 02:20:44 +0530

    Debugging.

--- src/main/java/onboarding/Service/CustomerService.java
+++ src/main/java/onboarding/Service/CustomerService.java
@@ -2,19 +2,23 @@ package onboarding.Service;
 
 import onboarding.entity.Customer;
 import onboarding.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
+@Transactional
 public class CustomerService {
 
-    private final CustomerRepository customerRepository;
-
-    public CustomerService(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
+    @Autowired
+    private CustomerRepository customerRepository;
 
     public Customer saveCustomer(Customer customer) {
         customer.setKycStatus("PENDING"); // Default status
         return customerRepository.save(customer);
     }
+
+
 }
